adf3c71dd4574a2b838dd5981c37c4762657e2d9
s_furuta <s_furuta@Kas-se-pc27.mshome.net>
2021-02-09 16:55:17 +0900

    第02回終了まで

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
+import v1_0.PokerGame;
 
 public class GameMain {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		Scanner stdIn = new Scanner(System.in); // 標準入出力を使用
+		
+		while ( true ) {
+			System.out.println("開始→1、終了→0");
+			System.out.print(">");
+			
+			int command = stdIn.nextInt();
+			if (command == 0) {
+				break;
+			}
+			/*
+			 * ゲーム1回分の処理
+			 */
+			PokerGame.exec(stdIn);
+		}
+		
+		System.out.println("終了しました。");
+		stdIn.close(); // 標準入出力を解放
 	}
 
 }
---
+++ src/v1_0/PokerGame.java
@@ -0,0 +1,47 @@
+package v1_0;
+
+import java.util.Scanner;
+
+public class PokerGame {
+	public static void exec(Scanner in) { // Game1回分の処理
+		// 初期化部
+		int[] pile = new int[52]; // 山札
+		int[] hand = new int[ 5]; // 手札
+		String[] card = new String[52]; // トランプの表面
+		String[] suit = {"♠", "♡", "♢", "♣"}; // トランプのマーク
+		String[] num  = {" A", " 2", " 3", " 4", " 5", " 6", " 7",
+						 " 8", " 9", "10", " J", " Q", " K"}; // トランプの数字部分
+		
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 13; j++) {
+				card[i * 13 + j] = suit[i] + num[j];
+			}
+		}
+		for (int i = 0; i < 52; i++) {
+			pile[i] = i;
+		}
+		
+		// 処理部
+		System.out.println(">>> Java Poker START <<<");
+		
+		for (int i = 0; i < 5; i++) {
+			hand[i] = pile[i];
+		}
+		
+		System.out.println("[" + card[ hand[0] ] + "]" + 
+		                   "[" + card[ hand[1] ] + "]" + 
+				           "[" + card[ hand[2] ] + "]" + 
+		                   "[" + card[ hand[3] ] + "]" + 
+				           "[" + card[ hand[4] ] + "]");
+		System.out.println("  1    2    3    4    5  ");
+		System.out.print("何枚替えますか？（0-5）>");
+		in.nextInt();
+		
+		System.out.println(">>> Java Poker  END  <<<");
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+}
